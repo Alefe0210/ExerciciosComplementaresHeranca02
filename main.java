612de518881aad65089b87921251522dcba79ba0
Alefe0210 <99697139+Alefe0210@users.noreply.github.com>
2023-07-10 02:05:17 -0300

    mn

---
+++ main.java
@@ -0,0 +1,13 @@
+package Empresa;
+
+import Class.Restaurante;
+
+public class main {
+    
+    public static void main(String[] args) {
+       Restaurante restaurante = new Restaurante();
+        restaurante.insertData();
+        System.out.println("\nDados do restaurante:");
+        restaurante.print();
+    }
+}
